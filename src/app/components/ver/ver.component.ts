5af8ca58e62c65ade5a917e2126ee35e393af22c
P4SCL <kidkros@hotmail.com>
2021-05-21 05:41:31 -0500

    implemetando uuid.V4 y boton download

--- src/app/components/ver/ver.component.ts
+++ src/app/components/ver/ver.component.ts
@@ -4,8 +4,13 @@ import { CertificadoI } from 'src/app/models/certificado.interface';
 import { CertificadoService } from '../../services/certificado.service';
 
 import { DomSanitizer } from '@angular/platform-browser';
-import { PdfMakeWrapper,Txt,Img,Columns} from 'pdfmake-wrapper';
+import { PdfMakeWrapper, Txt, Img, Columns } from 'pdfmake-wrapper';
 import pdfFonts from 'src/assets/custom-fonts';
+
+//UUID 
+import { v4 as uuidv4 } from 'uuid';
+
+
 
 
 @Component({
@@ -15,32 +20,32 @@ import pdfFonts from 'src/assets/custom-fonts';
 })
 export class VerComponent implements OnInit {
 
-  certificado:CertificadoI;
+  certificado: CertificadoI;
 
-  urlCertificado:any;
+  urlCertificado: any;
 
-  private logo:string = "https://firebasestorage.googleapis.com/v0/b/certificadoapp-3bb25.appspot.com/o/image%2Flogo_ho_opt.jpg?alt=media&token=d8ddc77d-6441-4af3-80ea-451e78624df5"
+  private logo: string = "https://firebasestorage.googleapis.com/v0/b/certificadoapp-3bb25.appspot.com/o/image%2Flogo_ho_opt.jpg?alt=media&token=d8ddc77d-6441-4af3-80ea-451e78624df5"
 
-  constructor(private activatedRoute:ActivatedRoute,
-              private certificadoService:CertificadoService,
-              private sanitizer: DomSanitizer) { }
+  constructor(private activatedRoute: ActivatedRoute,
+    private certificadoService: CertificadoService,
+    private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params=>{
+    this.activatedRoute.params.subscribe(params => {
       const id = params["id"];
-      this.certificadoService.getOneCertificado(id).subscribe(certificado=>{
+      this.certificadoService.getOneCertificado(id).subscribe(certificado => {
         this.generarPDF(certificado);
       });
     });
   }
 
-  async generarPDF(certificado){
-    const pdf = new PdfMakeWrapper();
+  async generarPDF(certificado) {
+    var pdf = new PdfMakeWrapper();
 
     //estilos de página
     pdf.pageOrientation('landscape');
     pdf.pageSize('A4');
-    
+
     pdf.pageMargins([50, 70]);
 
     //Background
@@ -49,7 +54,7 @@ export class VerComponent implements OnInit {
     //header logo
     pdf.header(await new Img(this.logo).build());
 
-    
+
     //titulo-h1
     pdf.add(new Txt('CERTIFICADO').fontSize(40).alignment('center').bold().end);
     // space
@@ -67,7 +72,7 @@ export class VerComponent implements OnInit {
     //body
     pdf.add(new Txt(`Por su participación como ${certificado.rol.toUpperCase()} en el workshop virtual de "${certificado.tema}" brindada por la instructor(a) ${certificado.encargado}.`).fontSize(20).alignment('center').end);
 
-  // spaces
+    // spaces
     pdf.add(new Txt('\n').end);
     pdf.add(new Txt('\n').end);
     pdf.add(new Txt('\n').end);
@@ -75,22 +80,28 @@ export class VerComponent implements OnInit {
     pdf.add(new Txt('\n').end);
     pdf.add(new Txt('\n').end);
 
-    pdf.add(new Columns(['________________________________________','________________________________________']).alignment('center').color('#135899').bold().end);
+    pdf.add(new Columns(['________________________________________', '________________________________________']).alignment('center').color('#135899').bold().end);
     pdf.add(new Txt('\n').end);
     pdf.add(
-      new Columns(['José Manuel Antonio Pachas','Kattya Isabel García Velasquez'])
-      .fontSize(15).alignment('center').end);
+      new Columns(['José Manuel Antonio Pachas', 'Kattya Isabel García Velasquez'])
+        .fontSize(15).alignment('center').end);
     pdf.add(
-      new Columns(['CEO DE CROWDEV','COO DE CROWDEV'])
-      .fontSize(13).alignment('center').end);
+      new Columns(['CEO DE CROWDEV', 'COO DE CROWDEV'])
+        .fontSize(13).alignment('center').end);
 
-    //Abre el pdf en otra ventana
-    //pdf.create().open();
-
-    pdf.create().getDataUrl(dataUrl=>{
+    pdf.create().getDataUrl(dataUrl => {
       this.urlCertificado = this.sanitizer.bypassSecurityTrustResourceUrl(dataUrl);
+      this.downloadPdf(dataUrl, certificado);
     });
-  
+  }
+
+  //Creando los atributos del ancla <a>
+  downloadPdf(url: any, certificado: any) {
+    //Generando UUID
+    const uuid = uuidv4();
+    const ancla = document.querySelector("#ancla_download");
+    ancla.setAttribute('href', url);
+    ancla.setAttribute('download', `${certificado?.nombres}-${uuid}`);
   }
 }
 
